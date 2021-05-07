f0897d7f90379e7c40684902bb1636572302e0e3
suryaprava <ashish.satapathy2009@gmail.com>
2021-05-07 22:02:28 +0530

    Spring MVC Demo Project

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-mvc-demo
+- This is a <b> Spring MVC </b> demo project.
---
+++ src/main/java/com/arsatapathy/spring/mvc/demo/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.arsatapathy.spring.mvc.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("home")
+    public String getHome() {
+        return "home";
+    }
+}
